326b342f583bf47b024b2b5eb85bb79aae53f992
kizup <puzikov@yksoft.ru>
2017-06-16 11:02:21 +0400

    Init commit

---
+++ android/src/ru/kizup/minibox2dgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.kizup.minibox2dgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.kizup.minibox2dgame.MiniGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MiniGame(), config);
+	}
+}
---
+++ desktop/src/ru/kizup/minibox2dgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.kizup.minibox2dgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.kizup.minibox2dgame.MiniGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MiniGame(), config);
+	}
+}
